c05d3ac35c2e165baa60b1bed910962165862484
sarahwambui96 <sarahwambui843@gmail.com>
2023-07-05 02:02:04 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
-from flask import request, session
+from flask import request, session, jsonify
 from flask_restful import Resource
 
 from config import app, db, api
 from models import User
+
+
 
 class ClearSession(Resource):
 
@@ -28,16 +30,43 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        if session.get("user_id"):
+            user = User.query.filter(User.id == session["user_id"]).first()
+            return user.to_dict(), 200
+        return {"message": "204: Not Authenticated"}, 204
 
 class Login(Resource):
-    pass
+    def post(self):
+        # user = User.query.filter(User.username == request.get_json()["username"]).first()
+        # session["user_id"] = user.id
+        # return jsonify(user.to_dict())
+    
+        username = request.get_json()['username']
+        password = request.get_json()['password']
 
+        user = User.query.filter(User.username == username).first()
+
+        if user.authenticate(password):
+
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+
+        return {'error': '401 Unauthorized'}, 401
+
+    
 class Logout(Resource):
-    pass
+    def delete(self):
+        session['user_id'] = None
+        return jsonify({'message': '204: No Content'}), 204
+    
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
